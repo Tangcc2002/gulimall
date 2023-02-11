9f687a78149bee36ec368e7430eb58ce76656d0c
16631 <2856440943@qq.com>
2023-02-11 18:22:04 +0800

    项目结构初始化

--- gulimall-product/src/test/java/com/atguigu/gulimallproduct/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimallproduct/GulimallProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.atguigu.gulimallproduct;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Resource
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setName("华为");
+
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
